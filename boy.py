40d3284678ae547f9cdd6dffa6feab8416c7627a
AN MIN YONG <iks05280204@gmail.com>
2025-10-16 19:59:17 +0900

    출력 크기 150,150 으로 더 크게 고정, 화면 끝에 가면 이동방향 바꾸기

--- boy.py
+++ boy.py
@@ -39,16 +39,19 @@ class AutoRun:
     def do(self):
         self.boy.frame = (self.boy.frame + 1) % 8
         self.boy.x += self.boy.dir * 5
-
+        if self.boy.dir == 1 and self.boy.x > 800:
+            self.boy.dir = self.boy.face_dir = -1
+        elif self.boy.dir == -1 and self.boy.x < 0:
+            self.boy.dir = self.boy.face_dir = 1
 
         if get_time() - self.boy.autorun_start_time > 5.0:
             self.boy.state_machine.handle_state_event(('TIMEOUT', None))
 
     def draw(self):
         if self.boy.face_dir == 1: # right
-            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y)
+            self.boy.image.clip_draw(self.boy.frame * 100, 100, 100, 100, self.boy.x, self.boy.y,150,150)
         else: # face_dir == -1: # left
-            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y)
+            self.boy.image.clip_draw(self.boy.frame * 100, 0, 100, 100, self.boy.x, self.boy.y,150,150)
 
 
 class RUN:
